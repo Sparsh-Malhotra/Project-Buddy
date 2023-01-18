8e72f08f85ef451020a84ec6c40ade401fdd1737
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2023-01-18 23:46:46 +0530

    Added favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -41,6 +41,25 @@ export default class MyDocument extends Document {
             crossOrigin=''
           />
           {CssBaseline.flush()}
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='192x192'
+            href='/android-chrome-192x192.png'
+          />
         </Head>
         <body>
           <Main />
