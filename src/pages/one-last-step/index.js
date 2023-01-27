e9195135746fcec59ca661f7ad640b3aaf7a13ac
Sparsh Malhotra <111490087+SparshMalhotra1@users.noreply.github.com>
2023-01-27 17:58:32 +0530

    Added Skills Array Dropdown

--- src/pages/one-last-step/index.js
+++ src/pages/one-last-step/index.js
@@ -80,8 +80,6 @@ const StyledAsyncSelect = styled(AsyncSelect)`
   }
 `;
 
-const StyledCheckbox = styled.div``;
-
 const genderOptions = [
   { value: "male", label: "Male" },
   { value: "female", label: "Female" },
@@ -96,6 +94,601 @@ const techStackOptions = [
   { value: "fullstack", label: "Full Stack" },
   { value: "ar/vr", label: "AR/VR" },
   { value: "Android/IOS", label: "android/ios" },
+];
+
+const skillsArray = [
+  {
+    label: "C",
+    value: "C",
+  },
+  {
+    label: "C++",
+    value: "C++",
+  },
+  {
+    label: "C#",
+    value: "C#",
+  },
+  {
+    label: "Python",
+    value: "Python",
+  },
+  {
+    label: "JavaScript",
+    value: "JavaScript",
+  },
+  {
+    label: "Ruby",
+    value: "Ruby",
+  },
+  {
+    label: "PHP",
+    value: "PHP",
+  },
+  {
+    label: "Swift",
+    value: "Swift",
+  },
+  {
+    label: "TypeScript",
+    value: "TypeScript",
+  },
+  {
+    label: "Kotlin",
+    value: "Kotlin",
+  },
+  {
+    label: "Go",
+    value: "Go",
+  },
+  {
+    label: "Scala",
+    value: "Scala",
+  },
+  {
+    label: "Rust",
+    value: "Rust",
+  },
+  {
+    label: "Dart",
+    value: "Dart",
+  },
+  {
+    label: "Shell",
+    value: "Shell",
+  },
+  {
+    label: "SQL",
+    value: "SQL",
+  },
+  {
+    label: "HTML",
+    value: "HTML",
+  },
+  {
+    label: "CSS",
+    value: "CSS",
+  },
+  {
+    label: "jQuery",
+    value: "jQuery",
+  },
+  {
+    label: "React",
+    value: "React",
+  },
+  {
+    label: "Angular",
+    value: "Angular",
+  },
+  {
+    label: "Vue.js",
+    value: "Vue.js",
+  },
+  {
+    label: "Ember.js",
+    value: "Ember.js",
+  },
+  {
+    label: "Backbone.js",
+    value: "Backbone.js",
+  },
+  {
+    label: "Meteor.js",
+    value: "Meteor.js",
+  },
+  {
+    label: "Svelte",
+    value: "Svelte",
+  },
+  {
+    label: "Next.js",
+    value: "Next.js",
+  },
+  {
+    label: "Nuxt.js",
+    value: "Nuxt.js",
+  },
+  {
+    label: "Gatsby",
+    value: "Gatsby",
+  },
+  {
+    label: "Webpack",
+    value: "Webpack",
+  },
+  {
+    label: "Grunt",
+    value: "Grunt",
+  },
+  {
+    label: "Gulp",
+    value: "Gulp",
+  },
+  {
+    label: "Node.js",
+    value: "Node.js",
+  },
+  {
+    label: "Express.js",
+    value: "Express.js",
+  },
+  {
+    label: "Ruby on Rails",
+    value: "Ruby on Rails",
+  },
+  {
+    label: "Django",
+    value: "Django",
+  },
+  {
+    label: "Flask",
+    value: "Flask",
+  },
+  {
+    label: "Laravel",
+    value: "Laravel",
+  },
+  {
+    label: "Spring",
+    value: "Spring",
+  },
+  {
+    label: "Yii",
+    value: "Yii",
+  },
+  {
+    label: "Zend Framework",
+    value: "Zend Framework",
+  },
+  {
+    label: "Swift",
+    value: "Swift",
+  },
+  {
+    label: "Objective-C",
+    value: "Objective-C",
+  },
+  {
+    label: "Java",
+    value: "Java",
+  },
+  {
+    label: "Kotlin",
+    value: "Kotlin",
+  },
+  {
+    label: "React-Native",
+    value: "React-Native",
+  },
+  {
+    label: "Flutter",
+    value: "Flutter",
+  },
+  {
+    label: "Xamarin",
+    value: "Xamarin",
+  },
+  {
+    label: "Ionic",
+    value: "Ionic",
+  },
+  {
+    label: "MySQL",
+    value: "MySQL",
+  },
+  {
+    label: "PostgreSQL",
+    value: "PostgreSQL",
+  },
+  {
+    label: "MongoDB",
+    value: "MongoDB",
+  },
+  {
+    label: "Oracle",
+    value: "Oracle",
+  },
+  {
+    label: "SQL Server",
+    value: "SQL Server",
+  },
+  {
+    label: "SQLite",
+    value: "SQLite",
+  },
+  {
+    label: "Firebase",
+    value: "Firebase",
+  },
+  {
+    label: "Cosmos DB",
+    value: "Cosmos DB",
+  },
+  {
+    label: "Cassandra",
+    value: "Cassandra",
+  },
+  {
+    label: "Redis",
+    value: "Redis",
+  },
+  {
+    label: "Elasticsearch",
+    value: "Elasticsearch",
+  },
+  {
+    label: "AWS",
+    value: "AWS",
+  },
+  {
+    label: "Azure",
+    value: "Azure",
+  },
+  {
+    label: "Google Cloud",
+    value: "Google Cloud",
+  },
+  {
+    label: "OpenStack",
+    value: "OpenStack",
+  },
+  {
+    label: "Heroku",
+    value: "Heroku",
+  },
+  {
+    label: "DigitalOcean",
+    value: "DigitalOcean",
+  },
+  {
+    label: "Alibaba Cloud",
+    value: "Alibaba Cloud",
+  },
+  {
+    label: "Windows",
+    value: "Windows",
+  },
+  {
+    label: "Linux",
+    value: "Linux",
+  },
+  {
+    label: "macOS",
+    value: "macOS",
+  },
+  {
+    label: "iOS",
+    value: "iOS",
+  },
+  {
+    label: "Android",
+    value: "Android",
+  },
+  {
+    label: "Windows Mobile",
+    value: "Windows Mobile",
+  },
+  {
+    label: "Blackberry",
+    value: "Blackberry",
+  },
+  {
+    label: "TCP/IP",
+    value: "TCP/IP",
+  },
+  {
+    label: "DNS",
+    value: "DNS",
+  },
+  {
+    label: "DHCP",
+    value: "DHCP",
+  },
+  {
+    label: "VPN",
+    value: "VPN",
+  },
+  {
+    label: "Load Balancing",
+    value: "Load Balancing",
+  },
+  {
+    label: "Firewall",
+    value: "Firewall",
+  },
+  {
+    label: "Routing",
+    value: "Routing",
+  },
+  {
+    label: "Switching",
+    value: "Switching",
+  },
+  {
+    label: "Wireless Network",
+    value: "Wireless Network",
+  },
+  {
+    label: "Firewalls",
+    value: "Firewalls",
+  },
+  {
+    label: "intrusion detection and prevention systems",
+    value: "intrusion detection and prevention systems",
+  },
+  {
+    label: "encryption",
+    value: "encryption",
+  },
+  {
+    label: "network security",
+    value: "network security",
+  },
+  {
+    label: "pen-testing",
+    value: "pen-testing",
+  },
+  {
+    label: "SIEM",
+    value: "SIEM",
+  },
+  {
+    label: "SOC",
+    value: "SOC",
+  },
+  {
+    label: "threat intelligence",
+    value: "threat intelligence",
+  },
+  {
+    label: "incident response",
+    value: "incident response",
+  },
+  {
+    label: "SQL",
+    value: "SQL",
+  },
+  {
+    label: "R",
+    value: "R",
+  },
+  {
+    label: "SAS",
+    value: "SAS",
+  },
+  {
+    label: "Python (Pandas, Numpy, Scikit-learn, Matplotlib, Seaborn)",
+    value: "Python (Pandas, Numpy, Scikit-learn, Matplotlib, Seaborn)",
+  },
+  {
+    label: "Excel",
+    value: "Excel",
+  },
+  {
+    label: "Tableau",
+    value: "Tableau",
+  },
+  {
+    label: "Power BI",
+    value: "Power BI",
+  },
+  {
+    label: "Python (TensorFlow, Keras, Scikit-learn, PyTorch)",
+    value: "Python (TensorFlow, Keras, Scikit-learn, PyTorch)",
+  },
+  {
+    label: "R (caret, mlr)",
+    value: "R (caret, mlr)",
+  },
+  {
+    label: "SAS",
+    value: "SAS",
+  },
+  {
+    label: "Spark MLlib",
+    value: "Spark MLlib",
+  },
+  {
+    label: "H2O.ai",
+    value: "H2O.ai",
+  },
+  {
+    label: "RapidMiner",
+    value: "RapidMiner",
+  },
+  {
+    label: "KNIME",
+    value: "KNIME",
+  },
+  {
+    label: "Orange",
+    value: "Orange",
+  },
+  {
+    label: "Weka",
+    value: "Weka",
+  },
+  {
+    label: "Jenkins",
+    value: "Jenkins",
+  },
+  {
+    label: "Travis CI",
+    value: "Travis CI",
+  },
+  {
+    label: "CircleCI",
+    value: "CircleCI",
+  },
+  {
+    label: "GitLab CI/CD",
+    value: "GitLab CI/CD",
+  },
+  {
+    label: "Ansible",
+    value: "Ansible",
+  },
+  {
+    label: "Docker",
+    value: "Docker",
+  },
+  {
+    label: "Kubernetes",
+    value: "Kubernetes",
+  },
+  {
+    label: "AWS ECS",
+    value: "AWS ECS",
+  },
+  {
+    label: "Azure Container Instance",
+    value: "Azure Container Instance",
+  },
+  {
+    label: "Unity",
+    value: "Unity",
+  },
+  {
+    label: "Unreal Engine",
+    value: "Unreal Engine",
+  },
+  {
+    label: "Godot",
+    value: "Godot",
+  },
+  {
+    label: "GameMaker Studio",
+    value: "GameMaker Studio",
+  },
+  {
+    label: "CryEngine",
+    value: "CryEngine",
+  },
+  {
+    label: "Construct 2",
+    value: "Construct 2",
+  },
+  {
+    label: "TensorFlow",
+    value: "TensorFlow",
+  },
+  {
+    label: "Keras",
+    value: "Keras",
+  },
+  {
+    label: "PyTorch",
+    value: "PyTorch",
+  },
+  {
+    label: "scikit-learn",
+    value: "scikit-learn",
+  },
+  {
+    label: "Caffe",
+    value: "Caffe",
+  },
+  {
+    label: "Theano",
+    value: "Theano",
+  },
+  {
+    label: "Torch",
+    value: "Torch",
+  },
+  {
+    label: "NLTK",
+    value: "NLTK",
+  },
+  {
+    label: "spaCy",
+    value: "spaCy",
+  },
+  {
+    label: "OpenCV",
+    value: "OpenCV",
+  },
+  {
+    label: "Raspberry Pi",
+    value: "Raspberry Pi",
+  },
+  {
+    label: "Arduino",
+    value: "Arduino",
+  },
+  {
+    label: "Node-RED",
+    value: "Node-RED",
+  },
+  {
+    label: "MQTT",
+    value: "MQTT",
+  },
+  {
+    label: "CoAP",
+    value: "CoAP",
+  },
+  {
+    label: "Zigbee",
+    value: "Zigbee",
+  },
+  {
+    label: "Z-Wave",
+    value: "Z-Wave",
+  },
+  {
+    label: "LoRa",
+    value: "LoRa",
+  },
+  {
+    label: "BLE",
+    value: "BLE",
+  },
+  {
+    label: "Unity",
+    value: "Unity",
+  },
+  {
+    label: "A-Frame",
+    value: "A-Frame",
+  },
+  {
+    label: "Babylon.js",
+    value: "Babylon.js",
+  },
+  {
+    label: "Three.js",
+    value: "Three.js",
+  },
+  {
+    label: "Vuforia",
+    value: "Vuforia",
+  },
+  {
+    label: "ARKit",
+    value: "ARKit",
+  },
 ];
 
 const Step1Content = ({ onChangeHandler }) => {
@@ -196,12 +789,14 @@ const Step2Content = ({ onChangeHandler }) => {
           placeholder='Preferred Tech Stack*'
           onChange={(e) => onChangeHandler("techStack", e.value)}
         ></StyledSelect>
-        <StyledInputBox
+        <StyledSelect
           id='skills'
-          type='text'
+          isMulti
+          className='react-select'
+          options={skillsArray}
           placeholder='Skills*'
-          onChange={(e) => onChangeHandler("skills", e.target.value)}
-        ></StyledInputBox>
+          onChange={(e) => onChangeHandler("skills", e)}
+        ></StyledSelect>
         <StyledInputBox
           id='linkedin'
           type='text'
@@ -257,8 +852,6 @@ const OneLastStep = () => {
   const dispatch = useDispatch();
   const router = useRouter();
 
-  const toggleCheck = () => setIsChecked((prevState) => !prevState);
-
   const onFormSubmit = async () => {
     const body = {
       firstName,
@@ -275,7 +868,6 @@ const OneLastStep = () => {
     };
     const res = await submitDetails(body, user.authToken);
     if (res.message === "Success") {
-      //   router.push("/user/sparsh");
       dispatch(updateAppState("LOGGED_IN"));
       const name = user.name.toLowerCase().replace(" ", "-");
       router.push(`/user/${name}`);
@@ -309,7 +901,7 @@ const OneLastStep = () => {
         setTechStack(payload);
         break;
       case "skills":
-        setSkills(payload.split(","));
+        setSkills(payload.map((ele) => ele.value));
         break;
       case "linkedin":
         setLinkedin(payload);
